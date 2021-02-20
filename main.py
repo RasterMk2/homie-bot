f0704ecf9bdf799632ccdb4537051a8ab9de7c23
RasterMk2 <70229194+RasterMk2@users.noreply.github.com>
2021-02-20 16:33:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def stats(ctx):
 
     embed.set_author(name=ctx.message.author, icon_url=ctx.author.avatar_url)
 
-    embed.add_field(name='Homie-Points <:homiepoint:812798235185774672:>', value=data.get([user, 'hp']), inline=False)
+    embed.add_field(name='Homie-Points :homiepoint:812798235185774672:', value=data.get([user, 'hp']), inline=False)
 
     embed.add_field(name='Level', value=lvl + 1, inline=False)
 
